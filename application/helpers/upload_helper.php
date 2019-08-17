defa2d139fb7422b33227736cf0f40a400359b92
Fata El Islami <fataelislami97@gmail.com>
2019-08-17 15:24:44 +0700

    Add upload helper

---
+++ application/helpers/upload_helper.php
@@ -0,0 +1,60 @@
+<?php
+/*Cara penggunaan Kostlab File Helper
+PASTIKAN FOLDER TUJUAN PERMISSION NYA SUDAH BISA WRITE
+dan
+PASTIKAN FORM SUDAH BERUBAH DENGAN enctype="multipart/form-data"
+Keterangan
+upload("Nama Form","Direktori Tujuan","Type File","Enkripsi Nama")
+
+contoh
+jika <input type="file" name="photo">
+dengan folder tujuan photo
+maka
+$photo=upload('photo','photo','image',TRUE);
+if($photo){
+  echo "foto berhasil di upload";
+}else{
+  echo "gagal upload foto";
+}
+
+Gunakan class ini untuk FORM create
+<input type="file" id="input-file-now-custom-1" class="dropify" name="file">
+
+Gunaka class ini untuk Form Edit, dan ubah default URL nya
+<input type="file" id="input-file-now-custom-1" class="dropify" data-default-file="<?php echo base_url().'xfile/{namafoldertujuan}'.$dataedit->photoUrl?>" name="file">
+
+*/
+
+ function upload($formname,$pathFolder,$type='image',$encrypt=FALSE){//default untuk image
+    $ci =& get_instance();//$ci untuk pengganti $this->
+    $config['upload_path']          = './xfile/'.$pathFolder;
+    if($type=='file'){
+      $config['allowed_types']        = 'pdf';
+      //$config['max_size']             = 100;
+    }else if($type=='audio'){
+      $config['allowed_types']        = 'mp3';
+      //$config['max_size']             = 100;
+    }else if($type=='image'){
+      $config['allowed_types']        = 'gif|jpg|png|jpeg';
+      //$config['max_size']             = 100;
+      //$config['max_width']            = 1024;
+      //$config['max_height']           = 768;
+    }else if($type=='video'){
+      $config['allowed_types']        = 'mp4|avi';
+      //$config['max_size']             = 100;
+    }
+    $config['overwrite'] = FALSE;
+    $config['encrypt_name'] = $encrypt;
+    $ci->load->library('upload', $config);
+    $ci->upload->initialize($config);
+    $exec=$ci->upload->do_upload($formname);
+    if($exec){
+      return $ci->upload->data();
+    }else{
+      return FALSE;
+    }
+ }
+
+
+
+ ?>
